8a9c227c7d28e52ca8a36f82239e4b908b1cf6ab
Jeet Kothari <73366861+JeetKothari908@users.noreply.github.com>
2026-02-14 15:19:54 -0600

    wallpapersssss

--- README.md
+++ README.md
@@ -4,9 +4,7 @@ Basic web extension for google chrome that changes your default new tab to have 
 The only thing that I can really even think of thats left to do is use more wallpapers. That is literally it. 
 
 To Do:
-3. Use more wallpapers
 4. Change how the check box looks when its checked off to look more like momentum
 5. Add a "finished" tasks, and along with, find some way of adding instanced of finished "repeated" tasks. With this list, there should be a "undo" button, that once pressed, readds the item to the list. 
 6. Getting the x icon working, and have that be labeled "finished" tasks and the garbage ones actually being deleted. 
-7. Figure out how it works for different window sizes. Base it off of momentum.
 8. Consider how keyboard behavior might affect how to do list things.
--- tabliss-main/src/plugins/backgrounds/wallpapers/Wallpapers.tsx
+++ tabliss-main/src/plugins/backgrounds/wallpapers/Wallpapers.tsx
@@ -3,13 +3,26 @@ import { useTime } from "../../../hooks";
 import Backdrop from "../../../views/shared/Backdrop";
 import "./Wallpapers.sass";
 
-import wallpaper1 from "../../../assets/wallpapers/wallpaper-1.jpg";
-import wallpaper2 from "../../../assets/wallpapers/wallpaper-2.jpg";
-import wallpaper3 from "../../../assets/wallpapers/wallpaper-3.jpg";
-import wallpaper4 from "../../../assets/wallpapers/wallpaper-4.jpg";
-import wallpaper5 from "../../../assets/wallpapers/wallpaper-5.jpg";
-import wallpaper6 from "../../../assets/wallpapers/wallpaper-6.jpg";
-import wallpaper7 from "../../../assets/wallpapers/wallpaper-7.jpg";
+import wallpaper1 from "../../../assets/wallpapers/105306-brown_and_white_concrete_building-2560x1440.jpg";
+import wallpaper2 from "../../../assets/wallpapers/127252-white_concrete_bridge_over_blue_sea_during_daytime-2560x1440.jpg";
+import wallpaper3 from "../../../assets/wallpapers/148905-brown_and_white_concrete_houses_during_daytime-2560x1440.jpg";
+import wallpaper4 from "../../../assets/wallpapers/149363-lighted_bridge_during_night_time-2560x1440.jpg";
+import wallpaper5 from "../../../assets/wallpapers/150994-white_and_black_temple_under_blue_sky_during_daytime-2560x1440.jpg";
+import wallpaper6 from "../../../assets/wallpapers/155563-stockholm-car_rental-city-water-building-2560x1440.jpg";
+import wallpaper7 from "../../../assets/wallpapers/162355-szchenyi_chain_bridge-body_of_water-bridge-water-water_resources-2560x1440.jpg";
+import wallpaper8 from "../../../assets/wallpapers/165485-taj_mahal-agra_fort-yamuna_river-new7wonders_of_the_world-monument-2560x1440.jpg";
+import wallpaper9 from "../../../assets/wallpapers/172074-eiffel_tower-les_invalides-arc_de_triomphe-montparnasse-tower-2560x1440.jpg";
+import wallpaper10 from "../../../assets/wallpapers/172325-golden_gate_bridge-muir_woods_national_monument-recreation-travel-landmark-2560x1440.jpg";
+import wallpaper11 from "../../../assets/wallpapers/175872-chinese_architecture-leipzig-architecture-cloud-daytime-2560x1440.jpg";
+import wallpaper12 from "../../../assets/wallpapers/179483-building-carterton-property-property_investment-house-2560x1440.jpg";
+import wallpaper13 from "../../../assets/wallpapers/85821-city_skyline_during_night_time-2560x1440.jpg";
+import wallpaper14 from "../../../assets/wallpapers/90846-aerial_view_of_city_buildings_during_night_time-2560x1440.jpg";
+import wallpaper15 from "../../../assets/wallpapers/90887-aerial_view_of_city_buildings_during_daytime-2560x1440.jpg";
+import wallpaper16 from "../../../assets/wallpapers/91694-white_and_gray_concrete_castle_on_top_of_green_mountain_during_daytime-2560x1440.jpg";
+import wallpaper17 from "../../../assets/wallpapers/92499-bridge_over_river_during_night_time-2560x1440.jpg";
+import wallpaper18 from "../../../assets/wallpapers/92602-brown_concrete_building_near_river_under_blue_sky_during_daytime-2560x1440.jpg";
+import wallpaper19 from "../../../assets/wallpapers/92709-people_walking_on_snow_covered_ground_near_white_and_black_concrete_building_during_daytime-2560x1440.jpg";
+import wallpaper20 from "../../../assets/wallpapers/93819-london_bridge-sky-arch_bridge-tower_of_london-cable_stayed_bridge-2560x1440.jpg";
 
 const wallpapers = [
   wallpaper1,
@@ -19,6 +32,19 @@ const wallpapers = [
   wallpaper5,
   wallpaper6,
   wallpaper7,
+  wallpaper8,
+  wallpaper9,
+  wallpaper10,
+  wallpaper11,
+  wallpaper12,
+  wallpaper13,
+  wallpaper14,
+  wallpaper15,
+  wallpaper16,
+  wallpaper17,
+  wallpaper18,
+  wallpaper19,
+  wallpaper20,
 ];
 
 const Wallpapers: React.FC = () => {
